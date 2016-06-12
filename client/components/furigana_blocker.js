081c6966e1c94beefda05d2a49eb64eaec06779b
Jack Preston <unwttng@gmail.com>
2016-06-12 15:22:59 +0900

    Furigana styling

--- client/components/furigana_blocker.js
+++ client/components/furigana_blocker.js
@@ -10,8 +10,8 @@ export default class FuriganaBlocker extends React.Component {
       <div
         className={classnames(c.furiganaBlocker, {[c.hidden]: !this.props.reader.state.furiganaShown})}
         style={{
-          backgroundColor: this.props.highlighted ? 'rgba(255, 0, 0, 0.5)' : this.props.furigana.color,
-          border: this.props.highlighted ? '2px solid rgb(255, 0, 0)' : null,
+          backgroundColor: this.props.highlighted ? 'rgba(255, 0, 0, 0.2)' : this.props.furigana.color,
+          border: this.props.highlighted ? '1px solid rgb(255, 0, 0)' : null,
           width: `${this.props.furigana.w}%`,
           height: `${this.props.furigana.h}%`,
           left: `${this.props.furigana.x}%`,
--- static/js/client.js
+++ static/js/client.js
@@ -37375,8 +37375,8 @@
 	      return _react2.default.createElement('div', {
 	        className: (0, _classnames3.default)(_furigana_blocker2.default.furiganaBlocker, _defineProperty({}, _furigana_blocker2.default.hidden, !this.props.reader.state.furiganaShown)),
 	        style: {
-	          backgroundColor: this.props.highlighted ? 'rgba(255, 0, 0, 0.5)' : this.props.furigana.color,
-	          border: this.props.highlighted ? '2px solid rgb(255, 0, 0)' : null,
+	          backgroundColor: this.props.highlighted ? 'rgba(255, 0, 0, 0.2)' : this.props.furigana.color,
+	          border: this.props.highlighted ? '1px solid rgb(255, 0, 0)' : null,
 	          width: this.props.furigana.w + '%',
 	          height: this.props.furigana.h + '%',
 	          left: this.props.furigana.x + '%',
